611c93a35b896f591f024d15d62ec90b677aa3be
Alex Kim <312akim@gmail.com>
2021-09-07 23:38:34 -0700

    Feature: Added filtering functionality to survey states for pie chart from server

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,66 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 import { useQuery, gql } from '@apollo/client';
 import { listSurveys } from '../graphql/queries';
 
-const testData = [
-    { modeOption: 'dark', stateOption: 'redux'},
-    { modeOption: 'dark', stateOption: 'mobx'},
-    { modeOption: 'light', stateOption: 'recoil'},
-    { modeOption: 'dark', stateOption: 'context'},
-    { modeOption: 'light', stateOption: 'redux'},
-    { modeOption: 'dark', stateOption: 'other'},
-    { modeOption: 'light', stateOption: 'other'},
-];
-
 function ChartComponent() {
-    const { loading, error, data } = useQuery(gql(listSurveys));
-    
-    // console.log(data.listSurveys.items);
-    
-    // const lightCount = data.listSurveys.items.filter((obj: any) => obj.modeOption === 'light');
-    // console.log(`length: ${lightCount.length}`);
+    // Queries all surveys every 5 seconds
+    const { loading, error, data } = useQuery(gql(listSurveys), {
+        pollInterval: 5000
+    });
+
+    // State
+    const [lightCount, setLightCount] = useState(0);
+    const [darkCount, setDarkCount] = useState(0);
+    const [contextCount, setContextCount] = useState(0);
+    const [reduxCount, setReduxCount] = useState(0);
+    const [mobxCount, setMobxCount] = useState(0);
+    const [recoilCount, setRecoilCount] = useState(0);
+    const [otherCount, setOtherCount] = useState(0);
+
+    // Once data received, update States
+    useEffect(() => {
+        if (!loading) {
+            setLightCount(data.listSurveys.items.filter((obj: any) => obj.modeOption === 'light').length);
+            setDarkCount(data.listSurveys.items.filter((obj: any) => obj.modeOption === 'dark').length);
+            setContextCount(data.listSurveys.items.filter((obj: any) => obj.stateOption === 'context').length);
+            setReduxCount(data.listSurveys.items.filter((obj: any) => obj.stateOption === 'redux').length);
+            setMobxCount(data.listSurveys.items.filter((obj: any) => obj.stateOption === 'mobx').length);
+            setRecoilCount(data.listSurveys.items.filter((obj: any) => obj.stateOption === 'recoil').length);
+            setOtherCount(data.listSurveys.items.filter((obj: any) => obj.stateOption === 'other').length);
+        }
+    }, [lightCount, darkCount, contextCount, reduxCount, mobxCount, recoilCount, otherCount, loading, data])
 
     return (
         <div style={styles.chartContainer}>
-            <PieChart
-                style={{
-                    overflow: 'visible',
-                    maxWidth: '343px',
-                }}
-                data={[
-                    {
-                        title: 'Redux',
-                        value: 25,
-                        color: 'red'
-                    }, 
-                    {
-                        title: 'MobX',
-                        value: 30,
-                        color: 'blue'
-                    }, 
-                    {
-                        title: 'Context',
-                        value: 45,
-                        color: 'yellow'
-                    },
-                    {
-                        title: 'Recoil',
-                        value: 15,
-                        color: 'green'
-                    },
-                    {
-                        title: 'Other',
-                        value: 3,
-                        color: 'orange'
-                    }
-                ]}
-                label={({dataEntry}) => `${dataEntry.title}`}
-                segmentsShift={(index)=> index === 0 ? 3.5 : 0}
-                labelStyle={{...defaultLabelStyle}}
-                lineWidth={40}
-                animate={true}
-            />
+            {
+                loading ? <div>Loading..</div> :
+                <>
+                    <h2>State Survey Results</h2>
+                    <PieChart
+                        style={{
+                            overflow: 'visible',
+                            maxWidth: '343px',
+                        }}
+                        
+                        data={[
+                            {
+                                title: 'Redux',
+                                value: reduxCount,
+                                color: 'red'
+                            }, 
+                            {
+                                title: 'MobX',
+                                value: mobxCount,
+                                color: 'blue'
+                            }, 
+                            {
+                                title: 'Context',
+                                value: contextCount,
+                                color: 'yellow'
+                            },
+                            {
+                                title: 'Recoil',
+                                value: recoilCount,
+                                color: 'green'
+                            },
+                            {
+                                title: 'Other',
+                                value: otherCount,
+                                color: 'orange'
+                            }
+                        ]}
+                        label={({dataEntry}) => `${dataEntry.title}`}
+                        segmentsShift={(index)=> index === 0 ? 3.5 : 0}
+                        labelStyle={{...defaultLabelStyle}}
+                        lineWidth={40}
+                        animate={true}
+                    />
+                </>
+            }
         </div>
     );
 }
